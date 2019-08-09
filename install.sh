f6c974b3a8b389edfca8d3c93041fe09c5d563cf
fivesmallq <fivesmallq@gmail.com>
2019-08-09 15:53:44 +0800

    init

---
+++ install.sh
@@ -0,0 +1,7 @@
+#curl -L -O https://github.com/theskumar/python-dotenv/archive/v0.10.3.zip
+#unzip v0.10.3.zip
+play_path=`dirname $(which play)`
+echo "$play_path"
+cp -r python-dotenv-0.10.3/src/dotenv $play_path"/framework/pym/"
+mv $play_path"/framework/pym/play/commands/base.py" $play_path"/framework/pym/play/commands/base.py_bak" 
+curl -sfL https://raw.githubusercontent.com/fivesmallq/play1-dotenv/master/base.py > $play_path"/framework/pym/play/commands/base.py"
